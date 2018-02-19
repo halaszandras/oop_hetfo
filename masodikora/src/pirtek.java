5922cbb33cb22414ca287e7d36c7207b2a100959
halasz13 <halasz13@lab104-06.iit.uni-miskolc.hu>
2018-02-19 16:40:28 +0100

    Faktorialis es Leibniz

--- masodikora/src/pirtek.java
+++ masodikora/src/pirtek.java
@@ -20,6 +20,31 @@ public class pirtek {
 		
 		System.out.println("pi = " +szam);
 		
+	//Leibniz-sor
+		
+		szam = 0;
+		int darab = 100000*4;
+		int elojel = 1;
+		
+		for (int i = 1; i <= darab; i+= 2)
+		{
+			szam += elojel * 1.0 / i;
+			elojel *= -1;
+		}
+		
+		szam*=4;
+		
+		System.out.println("pi = " +szam);	
+	
+	//Fuckt
+		
+		int szam2 = 10;
+		int Fucktor = 1;
+		
+		for (int i = 1; i <= szam2; i++) {
+			Fucktor *= i;
+			System.out.println(i + "!" + Fucktor);
+		}
 	}
 	
 }
